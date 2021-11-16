057d3086610c76d45c9ca77b3005400589e17464
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-16 17:28:22 +0100

    malloc nodes function edited to malloc size + 1

--- srcs/malloc/malloc_nodes.c
+++ srcs/malloc/malloc_nodes.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:22:20 by user42            #+#    #+#             */
-/*   Updated: 2021/11/15 16:24:03 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/16 17:26:32 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 nbr_data	*malloc_nodes(int size, nbr_data *node)
 {
-	node = (nbr_data *)malloc(sizeof(nbr_data) * size);
+	node = (nbr_data *)malloc(sizeof(nbr_data) * (size + 1));
 	if (node == NULL)
 		return (NULL);
 	return (node);
