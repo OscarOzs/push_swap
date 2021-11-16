ddae6363099264574a347f9d0f992ca303ce3369
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-16 20:52:08 +0100

    set_nodes.c file readapted to unsort nodes after setting up them

--- srcs/set_up/set_nodes.c
+++ srcs/set_up/set_nodes.c
@@ -6,13 +6,13 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 15:44:27 by user42            #+#    #+#             */
-/*   Updated: 2021/11/16 17:27:50 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/16 20:50:41 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sort_nodes(int size, nbr_data *node)
+static void	sort_nodes_and_get_position(int size, nbr_data *node)
 {
 	int tmp;
 	int	i;
@@ -32,22 +32,8 @@ static void	sort_nodes(int size, nbr_data *node)
 			}
 			j++;
 		}
+		node[i].nbr_pos = i;
 		i++;
-	}
-}
-
-static void	get_position(int size, nbr_data *node)
-{
-	int	i;
-	int	pos;
-
-	i = 0;
-	pos = 1;
-	while (i < size)
-	{
-		node[i].nbr_pos = pos;
-		i++;
-		pos++;
 	}
 }
 
@@ -79,14 +65,41 @@ static void	link_nodes(int size, nbr_data *node)
 	node[i].next = NULL;
 }
 
+static void	unsort_nodes(int size, char **arg, nbr_data *node)
+{
+	nbr_data	tmp;
+	int			i;
+	int			j;
+	int			k;
+
+	i = 1;
+	k = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (ft_atoi(arg[i]) == node[j].nbr)
+			{
+				tmp = node[j];
+				node[j] = node[k];
+				node[k] = tmp;
+			}
+			j++;
+		}
+		k++;
+		i++;
+	}
+}
+
 nbr_data	*set_nodes(int size, char **arg, nbr_data *node)
 {
 	node = malloc_nodes(size, node);
 	if (node == NULL)
 		return (NULL);
 	give_nbr_to_nodes(size, arg, node);
-	sort_nodes(size, node);
-	get_position(size, node);
+	sort_nodes_and_get_position(size, node);
+	unsort_nodes(size, arg, node);
 	link_nodes(size, node);
 	return (node);
 }
