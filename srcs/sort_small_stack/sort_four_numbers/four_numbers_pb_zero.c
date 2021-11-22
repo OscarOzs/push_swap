8e9e2ef27d67429e8f06b7792f895a9c30b8c99d
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-22 20:46:48 +0100

    zero is first function done, zero is seconde function started

--- srcs/sort_small_stack/sort_four_numbers/four_numbers_pb_zero.c
+++ srcs/sort_small_stack/sort_four_numbers/four_numbers_pb_zero.c
@@ -6,14 +6,56 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:32:30 by user42            #+#    #+#             */
-/*   Updated: 2021/11/22 17:36:55 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/22 20:29:47 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	four_numbers_pb_zero(nbr_data *number, nbr_data *stack_b)
+static nbr_data	*zero_is_first(nbr_data *number, nbr_data *stack_b)
 {
-	if (number[0].nbr_pos == 0)
-		
+	write(1, "pb\n", 3);
+	stack_b->nbr = number->nbr;
+	stack_b->nbr_pos = number->nbr_pos;
+	number = number->next;
+	return (number);
+}
+
+static nbr_data	*zero_is_second(nbr_data *number, nbr_data *stack_b)
+{
+	nbr_data	*tmp;
+
+	write(1, "ra\n", 3);
+	write(1, "pb\n", 3);
+	stack_b->nbr = number->nbr;
+	stack_b->nbr_pos = number->nbr_pos;
+
+	
+	tmp = number;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	number = number->next;
+	
+	tmp->next = number;
+	number->next = NULL;
+	return (number);
+}
+
+nbr_data	*four_numbers_pb_zero(nbr_data *number, nbr_data *stack_b)
+{
+	int			i;
+	nbr_data	*tmp;
+
+	tmp = number;
+	i = 0;
+	while (tmp != NULL)
+	{
+		if (tmp->nbr_pos == 0 && i == 0)
+			number = zero_is_first(number, stack_b);
+		else if (tmp->nbr_pos == 0 && i == 1)
+			number = zero_is_second(number, stack_b);
+		i++;
+		tmp = tmp->next;
+	}
+	return (number);
 }
--- srcs/sort_small_stack/sort_four_numbers/sort_four_numbers.c
+++ srcs/sort_small_stack/sort_four_numbers/sort_four_numbers.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:05:39 by user42            #+#    #+#             */
-/*   Updated: 2021/11/22 17:39:45 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/22 20:23:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,28 @@ void	sort_four_numbers(nbr_data *number)
 	nbr_data *stack_b;
 
 	stack_b = NULL;
-	stack_b = malloc_stack_b(number, stack_b);
+	stack_b = malloc(sizeof(nbr_data) * 1);
 	if (stack_b == NULL)
 	{
 		free_number_and_stack_b(number, stack_b);
 		exit(EXIT_FAILURE);
 	}
-	four_numbers_pb_zero(number, stack_b);
-	
+	stack_b->next = NULL;
+
+	nbr_data *tmp;
+
+	tmp = number;
+	while (number->next != NULL)
+	{
+		PRINTD(number->nbr_pos)
+		number = number->next;
+	}
+	number = tmp;
+	number = four_numbers_pb_zero(number, stack_b);
+	printf("\n\n");
+	while (number->next != NULL)
+	{
+		PRINTD(number->nbr_pos)
+		number = number->next;
+	}
 }
