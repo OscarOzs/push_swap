7d5952fdb3da94c7ed21b63abb9628b60aed6320
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 20:50:08 +0100

    sort_three_numbers function work with every operations function

--- srcs/operations/sa.c
+++ srcs/operations/sa.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:34:57 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 18:51:14 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 20:37:41 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 nbr_data	*sa(nbr_data *stack_a)
 {
-	nbr_data	*second_node;
+	nbr_data	*tmp;
 
 	if (lst_size(stack_a) < 2)
 		return (stack_a);
-	second_node = stack_a->next;
-	second_node->next = stack_a;
-	stack_a->next = NULL;
-	stack_a = second_node;
+	tmp = stack_a;
+	stack_a = stack_a->next;
+	tmp->next = stack_a->next;
+	stack_a->next = tmp;
 	write (1, "sa\n", 3);
 	return (stack_a);
 }
--- srcs/sort_small_stack/sort_three_numbers/sort_three_numbers.c
+++ srcs/sort_small_stack/sort_three_numbers/sort_three_numbers.c
@@ -6,24 +6,24 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 16:20:23 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 17:23:34 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 20:41:09 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void last_is_top(nbr_data *number)
+static nbr_data *last_is_top(nbr_data *number)
 {
 	if (number->next->nbr_pos == 1)
 	{
 		number = sa(number);
 		number = rra(number);
 	}
-	else if (number->next->next == 0)
+	else if (number->next->nbr_pos == 0)
 		number = ra(number);
 }
 
-static void	middle_is_top(nbr_data *number)
+static nbr_data	*middle_is_top(nbr_data *number)
 {
 	if (number->next->nbr_pos == 0)
 		number = sa(number);
@@ -31,11 +31,10 @@ static void	middle_is_top(nbr_data *number)
 		number = rra(number);
 }
 
-static void	first_is_top(nbr_data *number, nbr_data *stack_b)
+static nbr_data	*first_is_top(nbr_data *number, nbr_data *stack_b)
 {
-	number = pb(number, stack_b);
+	number = rra(number);
 	number = sa(number);
-	number = pa(number, stack_b);
 }
 
 nbr_data	*sort_three_numbers(nbr_data *number)
@@ -51,10 +50,10 @@ nbr_data	*sort_three_numbers(nbr_data *number)
 	}
 	stack_b->next = NULL;
 	if (number->nbr_pos == 2)
-		last_is_top(number);
+		number = last_is_top(number);
 	else if (number->nbr_pos == 1)
-		middle_is_top(number);
+		number  = middle_is_top(number);
 	else if (number->nbr_pos == 0)
-		first_is_top(number, stack_b);
+		number = first_is_top(number, stack_b);
 	return (number);
 }
