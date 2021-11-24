04e2d9393392126e76302d1ea9075ff0c1098e1a
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 19:42:39 +0100

    ra function work now

--- srcs/operations/ra.c
+++ srcs/operations/ra.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:20:40 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 15:43:56 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 19:41:52 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 nbr_data	*ra(nbr_data *stack_a)
 {
 	nbr_data	*last_node;
-	nbr_data	*first_node;
+	nbr_data	*tmp;
 
 	if (lst_size(stack_a) < 2)
 		return (stack_a);
 	last_node = stack_a;
-	first_node = stack_a;
 	while (last_node->next != NULL)
 		last_node = last_node->next;
+	tmp = stack_a;
 	stack_a = stack_a->next;
-	last_node->next = first_node;
-	first_node->next = NULL;
+	last_node->next = tmp;
+	tmp->next = NULL;
 	write(1, "ra\n", 3);
 	return (stack_a);
 }
