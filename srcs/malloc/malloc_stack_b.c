e8ade2e432cfb9e8e3c1612c1045c843adbab183
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 21:46:06 +0100

    malloc typecasted

--- srcs/malloc/malloc_stack_b.c
+++ srcs/malloc/malloc_stack_b.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:34:09 by user42            #+#    #+#             */
-/*   Updated: 2021/11/22 17:37:38 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 21:45:45 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ nbr_data	*malloc_stack_b(nbr_data *number, nbr_data *stack_b)
 	size_t	size;
 
 	size = lst_size(number);
-	stack_b = malloc(sizeof(nbr_data) * (size + 1));
+	stack_b = (nbr_data *)malloc(sizeof(nbr_data) * (size + 1));
 	if (stack_b == NULL)
 		return (NULL);
 	stack_b[size].next = NULL;
