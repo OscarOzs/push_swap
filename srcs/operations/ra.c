f63a3631580cd9ec2b6e02f865e8713b044238f0
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 15:45:01 +0100

    ra and rra functions protected from stack a has less than 2 nodes

--- srcs/operations/ra.c
+++ srcs/operations/ra.c
@@ -6,24 +6,26 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:20:40 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 15:01:12 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 15:43:56 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-nbr_data	*ra(nbr_data *stack)
+nbr_data	*ra(nbr_data *stack_a)
 {
 	nbr_data	*last_node;
 	nbr_data	*first_node;
 
-	last_node = stack;
-	first_node = stack;
+	if (lst_size(stack_a) < 2)
+		return (stack_a);
+	last_node = stack_a;
+	first_node = stack_a;
 	while (last_node->next != NULL)
 		last_node = last_node->next;
-	stack = stack->next;
+	stack_a = stack_a->next;
 	last_node->next = first_node;
 	first_node->next = NULL;
 	write(1, "ra\n", 3);
-	return (stack);
+	return (stack_a);
 }
--- srcs/operations/rra.c
+++ srcs/operations/rra.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 15:20:48 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 15:38:41 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 15:43:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ nbr_data	*rra(nbr_data *stack_a)
 	nbr_data	*last_node;
 	nbr_data	*before_last_node;
 
+	if (lst_size(stack_a) < 2)
+		return (stack_a);
 	before_last_node = stack_a;
 	while (before_last_node->next->next != NULL)
 		before_last_node = before_last_node->next;
