6e78a6a7d38597956726d9fe250b08c0bbe6d063
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 15:34:40 +0100

    rra function added

--- includes/operations.h
+++ includes/operations.h
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:21:47 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 14:22:09 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 15:22:16 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define OPERATIONS_H
 
 nbr_data	*ra(nbr_data *stack);
+nbr_data	*pb(nbr_data *stack_a, nbr_data *stack_b);
+nbr_data	*rra(nbr_data *stack_a);
 
 #endif
