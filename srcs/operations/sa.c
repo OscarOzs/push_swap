67e8704f2e1334c73f87abc242d56dfa1bf3e209
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-24 18:53:04 +0100

    function simplified

--- srcs/operations/sa.c
+++ srcs/operations/sa.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:34:57 by user42            #+#    #+#             */
-/*   Updated: 2021/11/24 18:40:36 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/24 18:51:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 nbr_data	*sa(nbr_data *stack_a)
 {
 	nbr_data	*second_node;
-	nbr_data	*tmp;
 
 	if (lst_size(stack_a) < 2)
 		return (stack_a);
-	tmp = stack_a;
-	tmp->nbr = second_node->nbr;
-	tmp->nbr_pos = second_node->nbr_pos;
-	second_node->nbr = stack_a->nbr;
-	second_node->nbr_pos = stack_a->nbr_pos;
-	stack_a = tmp;
+	second_node = stack_a->next;
+	second_node->next = stack_a;
+	stack_a->next = NULL;
+	stack_a = second_node;
 	write (1, "sa\n", 3);
 	return (stack_a);
 }
