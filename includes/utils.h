8b9bf73a6d3d618ead45448c54f06cf8f09ead07
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-30 16:55:48 +0100

    function show list added

--- includes/utils.h
+++ includes/utils.h
@@ -6,13 +6,14 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:40:45 by user42            #+#    #+#             */
-/*   Updated: 2021/11/29 17:44:54 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/30 16:23:57 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
+void	show_list(nbr_data	*lst);
 size_t	lst_size(nbr_data *lst);
 t_bool	is_lst_sorted(nbr_data *lst);
 void	swap_nodes(nbr_data **node1, nbr_data **node2);
