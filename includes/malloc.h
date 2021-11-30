0f7b962725f696075954b48eac338ccfa2369800
WilliamsOz <williams.ozserttas05@gmail.com>
2021-11-30 16:55:26 +0100

    function malloc_stack replace malloc_stack_a and malloc_stack_b

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,15 +6,14 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:35:59 by user42            #+#    #+#             */
-/*   Updated: 2021/11/29 23:39:50 by user42           ###   ########.fr       */
+/*   Updated: 2021/11/30 15:28:50 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MALLOC_H
 # define MALLOC_H
 
-nbr_data	*malloc_stack_a(int size, nbr_data *stack_a);
-nbr_data	*malloc_stack_b(nbr_data *stack_a, nbr_data *stack_b);
+nbr_data	*malloc_stack(int size, nbr_data *stack);
 void		free_stack(nbr_data *stack);
 
 #endif
