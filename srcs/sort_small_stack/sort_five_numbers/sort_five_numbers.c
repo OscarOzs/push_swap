39dd7648f72d411648e9f5c76b308fd7f6c9c918
Oscar OZSERTTAS <oozsertt@e3r3p18.clusters.42paris.fr>
2021-12-17 18:38:33 +0100

    problem with range 5 resolved

--- srcs/sort_small_stack/sort_five_numbers/sort_five_numbers.c
+++ srcs/sort_small_stack/sort_five_numbers/sort_five_numbers.c
@@ -6,7 +6,7 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:33:31 by oozsertt          #+#    #+#             */
-/*   Updated: 2021/12/17 17:54:36 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/12/17 18:34:39 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ t_nbr_data	*sort_five_numbers(t_nbr_data *stack_a)
 		return (stack_a);
 	stack_a = sort_three_numbers_left(stack_a);
 	stack_a = pa(stack_a, &stack_b);
-	stack_a = pa(stack_a, &stack_b);
+	if (lst_size(stack_b) == 1)
+		stack_a = pa(stack_a, &stack_b);
 	return (stack_a);
 }
